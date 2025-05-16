e92409684ffcd4c772427ff956bf5921c951c6dd
Somatra <cheysomatra@email.com>
2025-05-16 22:03:05 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// document.getElementById("myP").textContent = `Hello World!`;
+
+let x;
+x = 100;
+let firstName = "John";
+
+console.log(`X = $${x}`);
+console.log(typeof x)
+console.log(`Your first name is ${firstName}`);
